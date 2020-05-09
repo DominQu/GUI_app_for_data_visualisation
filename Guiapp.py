ac5491f2f1922804e49467b07e7369a68c816034
Dominik <dominik.kuczkowski00@gmail.com>
2020-05-09 19:08:49 +0200

    remove button

--- Guiapp.py
+++ Guiapp.py
@@ -29,13 +29,13 @@ chk = Checkbutton(window, text='Choose', var=chk_state)
 chk.grid(column=3, row=0)
 rad1 = Radiobutton(window,text='First', value=1)
 
-rad2 = Radiobutton(window,text='Second', value=2)
+
 
 rad3 = Radiobutton(window,text='Third', value=3)
 
 rad1.grid(column=0, row=1)
 
-rad2.grid(column=1, row=1)
+
 
 rad3.grid(column=2, row=1)
 window.mainloop()
