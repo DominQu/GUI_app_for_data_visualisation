d93031cc351540eaea3aa903d9fde26a66272e87
Dominik <dominik.kuczkowski00@gmail.com>
2020-05-10 21:52:01 +0200

    read and process data

--- projekt_3.py
+++ projekt_3.py
@@ -1,7 +1,14 @@
 import numpy as np
 
 filepath = "/home/dominik/Pulpit/data/outputPendulumOrt02.log"
+#filepath = "C:/Users/Dominik/Desktop/outputPendulumOrt02.log"
 
-#uploading and reading data
+#reading and processing data
 with open(filepath, "r") as file:
     data = file.readlines()
+numbers = []
+for line in data:
+    singlel = line.split()
+    singlel = [float(z) for z in singlel]
+    numbers.append(singlel)
+sensorval = np.array(numbers)   
